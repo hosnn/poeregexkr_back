f496fcb10aa81c406abdf39c0dd41e633fcecd06
unknown <soghtmd@gmail.com>
2024-12-29 17:29:03 +0900

    거래소 검색용 완성

---
+++ application.py
@@ -0,0 +1,3 @@
+from apps.app import create_app
+
+application = create_app()
--- apps/app.py
+++ apps/app.py
@@ -4,7 +4,19 @@ from flask_cors import CORS
 def create_app():
   app = Flask(__name__)
 
-  CORS(app)
+  # 특정 도메인만 CORS 허용
+  CORS(app, resources={
+    r"/*": {  
+        "origins": [
+          "https://poeregexkr.web.app",
+          "http://localhost:3000",
+        ]
+    }
+  })
+
+  @app.route('/')
+  def index():
+    return 'test', 200
 
   from apps.poe2api import views as poe2api_views
 
